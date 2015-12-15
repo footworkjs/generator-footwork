9529fd73e1fb7b5f004a28477416c8f368491390
Jonathan Newman <jonathan@overfoc.us>
2015-12-15 08:58:16 -0600

    initial working viewModel test with loaded fixture

--- generators/app/templates/tests/base.spec.js
+++ generators/app/templates/tests/base.spec.js
@@ -1,15 +1,23 @@
-define(['footwork'], function(fw) {
+define(['footwork', 'assets'], function(fw) {
 
   describe('just checking', function() {
     beforeEach(function(){
       fixture.setBase('tests/fixtures');
     });
 
-    it('works for app', function() {
-      var ExampleViewModelFixture = fixture.load('ExampleViewModel.html');
-      console.log(ExampleViewModelFixture);
-      expect(1).toEqual(1);
-      // fw.start();
+    afterEach(function(){
+      fixture.cleanup()
+    });
+
+    it('works for app', function(done) {
+      $(document.body).append(fixture.load('ExampleViewModel.html'));
+      fw.start();
+
+      setTimeout(function() {
+        expect($('.someValue').text()).toEqual('testValue');
+        done();
+      }, 20);
     });
   });
+
 });
--- generators/app/templates/tests/test-main.js
+++ generators/app/templates/tests/test-main.js
@@ -1,27 +1,28 @@
 var allTestFiles = [];
-var TEST_REGEXP = /(spec|test)\.js$/i;
-
-var pathToModule = function(path) {
-  var returnValue = path.replace(/^\/base\//, '').replace(/\.js$/, '');
-  return returnValue;
-};
+var TESTFILENAME_REGEXP = /(spec|test)\.js$/i;
 
 Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
+  if (TESTFILENAME_REGEXP.test(file)) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\//, '').replace(/\.js$/, '');
+    var normalizedTestModule = file.replace(/^\/base\//, '../../').replace(/\.js$/, '');
     allTestFiles.push(normalizedTestModule);
   }
 });
 
-REQUIRECONFIG.baseUrl = '/base/';
+var parentDir = /^\.\.\//;
+
+REQUIRECONFIG.baseUrl = '/base/public/' + REQUIRECONFIG.baseUrl;
 REQUIRECONFIG.deps = [REQUIRECONFIG.deps || []].concat(allTestFiles);
 REQUIRECONFIG.callback = window.__karma__.start;
 for (var property in REQUIRECONFIG.paths) {
   if (REQUIRECONFIG.paths.hasOwnProperty(property)) {
-    REQUIRECONFIG.paths[property] = REQUIRECONFIG.paths[property].replace(/^\.\.\//, 'public/');
+    if(REQUIRECONFIG.paths[property].match(parentDir)) {
+      REQUIRECONFIG.paths[property] = REQUIRECONFIG.paths[property].replace(parentDir, '../');
+    } else {
+      REQUIRECONFIG.paths[property] = REQUIRECONFIG.paths[property];
+    }
   }
 }
 
