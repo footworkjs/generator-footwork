3a1216340ecddb4c1c8e7bd80d44b2a1b7145b45
Jonathan Newman <jonathan@overfoc.us>
2015-12-30 17:27:59 -0600

    added /r/programming, now rendering subreddit data appropriately, updated colors/etc

--- generators/app/templates/public/app/component/subreddit/subreddit.js
+++ generators/app/templates/public/app/component/subreddit/subreddit.js
@@ -1,9 +1,37 @@
-define(["footwork"],
-  function(fw) {
+define(["footwork", "lodash"],
+  function(fw, _) {
+
+    var subreddit = fw.observable().receiveFrom('router', 'subreddit');
+
+    var SubRedditPostsCollection = fw.collection.create({
+      url: function() {
+        return 'https://www.reddit.com/r/' + subreddit() + '/.json?jsonp=getJSON';
+      },
+      parse: function(response) {
+        return response.data.children.map(function(rowData) {
+          rowData.data.selftext_html = _.unescape(rowData.data.selftext_html);
+          return rowData.data;
+        });
+      }
+    });
+
     return fw.viewModel.create({
-      namespace: 'Navigation',
+      namespace: 'subreddit',
       initialize: function() {
-        this.me = fw.observable('my-component');
+
+        var self = this;
+        this.posts = new SubRedditPostsCollection();
+
+        function getRedditPosts() {
+          subreddit() && self.posts.fetch({
+            jsonpCallback: 'getJSON',
+            dataType: 'jsonp'
+          });
+        }
+        getRedditPosts();
+        this.subredditSub = subreddit.subscribe(getRedditPosts);
+
+        window.subreddit = this;
       }
     });
   }
